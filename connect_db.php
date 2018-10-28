18d9b213405020e66ef200082a4bd1d5370d2cde
sonali0805 <sonalisrivastava1234@gmail.com>
2018-10-28 17:24:31 +0530

    change sql cred.

--- connect_db.php
+++ connect_db.php
@@ -1,8 +1,8 @@
 <?php
 $mysql_err = 'could not connect';
-$mysql_host = 'localhost';
-$mysql_user = 'root';
-$mysql_pass = '';
+$mysql_host = 'db4free.net';
+$mysql_user = 'sonali1234';
+$mysql_pass = '123456789';
 $mysql_db = 'hostle_detail';
 
 $conn = mysqli_connect($mysql_host,$mysql_user,$mysql_pass,$mysql_db);
