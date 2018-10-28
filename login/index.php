f0b1a77612ca0433973f5c7935e3a87d6047403b
sonali0805 <sonalisrivastava1234@gmail.com>
2018-10-28 21:43:53 +0530

    same button modification

--- login/index.php
+++ login/index.php
@@ -133,7 +133,7 @@
      <a href="./student/student_login_form.php"><button type="button" class="btn btn-success"><span style="font-size:3.5vh;font-weight:550;letter-spacing:2px;">I'M STUDENT</span></button></a>	
    </div>
    <div style=" float:left; display:inline; text-align: center; width:50%; height:100%; ">
-     <a href="./warden/warden_login_form.php" type="button" class="button"><b>I'M WARDEN</b></a>	
+     <a href="./warden/warden_login_form.php"><button type="button" class="btn btn-success"><span style="font-size:3.5vh;font-weight:550;letter-spacing:2px;">I'M WARDEN</span></button></a>	
    </div>
  </div>
 
